42e96799da1427cf0d693c70bd48c1615bdcf7c4
Josh Cheek <josh.cheek@gmail.com>
2016-11-05 14:27:45 -0500

    Refactor toplevel sib package a bit more

--- src/seeing_is_believing.js
+++ src/seeing_is_believing.js
@@ -1,29 +1,32 @@
 'use strict'
 
-const util = require('util');
-const vm   = require('vm');
-const wrap = require('./seeing_is_believing/wrap')
+const inspect = require('util').inspect
+const runCode = require('vm').runInNewContext
+const wrap    = require('./seeing_is_believing/wrap')
 
 // Events are snake case to keep the interface consistent with the Ruby implementation
 // I think there's an event API for node that I should look into rather than remaking my own
-
 module.exports = SeeingIsBelieving
+
 function SeeingIsBelieving({code, handler}) {
+  runCode(rewriteCode(code), sandboxFor(handler))
+  handler(['finished'])
+}
 
-  const script = new vm.Script(
-    wrap(code, (ast, line) => ast.update(`record(${line}, ${ast.source()})`))
-  )
+function rewriteCode(code) {
+  return wrap(code, (ast, lineNum) => {
+    ast.update(`record(${lineNum}, ${ast.source()})`)
+  })
+}
 
-  const sandbox = {
+function sandboxFor(handler) {
+  return {
     record: function record(lineNum, value) {
       handler(['line_result', {
         line_number: lineNum,
-        inspected:   util.inspect(value),
+        inspected:   inspect(value),
       }])
       return value
-    }
+    },
   }
-
-  script.runInNewContext(sandbox)
-  handler(['finished'])
 }
