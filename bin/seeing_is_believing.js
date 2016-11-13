54f9093e81733948f5b0ff1ceaebf99765b515c1
Josh Cheek <josh.cheek@gmail.com>
2016-11-13 17:15:57 -0600

    Binary reads the file and prints it

--- bin/seeing_is_believing.js
+++ bin/seeing_is_believing.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
-console.log("omg")
+const fs          = require('fs')
+const programName = process.argv[1]
+const args        = process.argv.slice(2)
+const inputFile   = args[0]
+
+fs.readFile(inputFile, 'utf8', (err, data) => {
+  console.log(data)
+})
+
+// // this one reads in stdin and writes it to stdout
+// let input = ""
+// process.stdin.on('data', data => input += data)
+// process.stdin.on('end', () => {
+//   process.stdout.write(input)
+// })
